cc406fda4a5a99db0bda332c59b7ad8ba064e074
nanemo <nanemomain@gmail.com>
2024-09-18 07:06:01 +0300

    271. Encode and Decode Strings. https://leetcode.com/problems/encode-and-decode-strings/

--- src/array_hashing/271_Decode_and_endcode_strings.kt
+++ src/array_hashing/271_Decode_and_endcode_strings.kt
@@ -1,2 +1,31 @@
-package ArrayHashing
+package array_hashing
 
+class Codec {
+    // Encodes a list of strings to a single string.
+    fun encode(strs: List<String>): String {
+        val encodedString = StringBuilder()
+        for (str in strs) {
+            encodedString.append(str.length)
+            encodedString.append("/")
+            encodedString.append(":")
+            encodedString.append(str)
+        }
+        println(encodedString)
+        return encodedString.toString()
+    }
+
+    // Decodes a single string to a list of strings.
+    fun decode(s: String): List<String> {
+        val decodedStrings = mutableListOf<String>()
+        var index = 0
+
+        while (index < s.length) {
+            val delimiter = s.indexOf(string = "/:", startIndex = index)
+            val wordSize = s.substring(startIndex = index, endIndex = delimiter).toInt()
+            val word = s.substring(delimiter + 2, wordSize + delimiter + 2)
+            decodedStrings.add(word)
+            index = wordSize + delimiter + 2
+        }
+        return decodedStrings.toList()
+    }
+}
