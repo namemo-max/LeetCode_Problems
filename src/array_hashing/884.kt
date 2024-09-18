f0c33d3275863d84338d7dc33c6e5c57727b9719
nanemo <nanemomain@gmail.com>
2024-09-18 11:24:51 +0300

    884

---
+++ src/array_hashing/884.kt
@@ -0,0 +1,23 @@
+package array_hashing
+
+/*884. Uncommon Words from Two Sentences*/
+
+class Solution {
+    fun uncommonFromSentences(s1: String, s2: String): Array<String> {
+        val map = HashMap<String, Int>()
+        val strs = s1.split(" ") + s2.split(" ")
+        val result = arrayListOf<String>()
+
+        for (str in strs) {
+            map.merge(str, 1, Int::plus)
+        }
+
+        for ((key, value) in map) {
+            if (value < 2) {
+                result.add(key)
+            }
+        }
+
+        return result.toTypedArray()
+    }
+}
