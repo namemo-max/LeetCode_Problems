55083e650dd187a900eed62e0555bc9a504d4337
nanemo <nanemomain@gmail.com>
2024-09-23 18:33:54 +0300

    //

---
+++ src/array_hashing/15.kt
@@ -0,0 +1,31 @@
+package array_hashing
+
+import java.util.*
+
+fun threeSum(nums: IntArray): List<List<Int>> {
+    val res = mutableListOf<List<Int>>()
+    Arrays.sort(nums)
+
+    for (i in 0..nums.size) {
+        if (i < nums.size && nums[i] <= 0) {
+            var left = i + 1
+            var right = nums.size - 1
+
+            while (left < right) {
+                val sum = nums[left] + nums[right] + nums[i]
+
+                when {
+                    sum < 0 -> ++left
+                    sum > 0 -> --right
+                    else -> {
+                        val list = listOf(nums[left++], nums[right--], nums[i])
+                        if (!res.contains(list)) {
+                            res.add(list)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return res
+}
---
+++ src/array_hashing/167.kt
@@ -0,0 +1,15 @@
+package array_hashing
+
+fun twoSum(numbers: IntArray, target: Int): IntArray {
+    var left = 0
+    var right = numbers.size - 1
+    while (left != right) {
+        val sum = numbers[left] + numbers[right]
+        when {
+            sum > target -> --right
+            sum < target -> ++left
+            else -> return intArrayOf(left + 1, right + 1)
+        }
+    }
+    return intArrayOf()
+}
